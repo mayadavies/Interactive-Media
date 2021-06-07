2f7e74af1d337e08ed5c10da3ccdbe54a3094237
mayadavies <mayahd.davies@gmail.com>
2021-06-07 13:45:39 -0400

    update

--- practice/jquery tutorial/script-tutorial/java.js
+++ practice/jquery tutorial/script-tutorial/java.js
@@ -1,3 +1,17 @@
-console.log("hello world");
+/*
+Abstract website tutorial
 
-$("body").css("background-color","green");
+
+example of writing code to create an abstract site based on 
+//http://www.abstractbrowsing.net//
+
+*/
+//this is a test to see if my file is linked tomy html//
+ 
+ console.log("hello world");
+//change bg color to green/
+
+ $("body").css("background-color","green");
+
+
+//command '/'' makea a whole line a comment//
