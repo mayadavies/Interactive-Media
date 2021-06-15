2e9c540349a0b9c1ab66fdc885077e9a60455253
mayadavies <mayahd.davies@gmail.com>
2021-06-14 22:53:43 -0400

    updt

--- projects/abstract-site/script/jquery.js
+++ projects/abstract-site/script/jquery.js
@@ -64,55 +64,6 @@ $("#box-container div").each(function(){
 // $(this).css("width",randomWidth).css("height", randomHeight);
 
 
-// });
-
-/*this is for resizing as it expands*/
-
-// $( window ).resize(function() {
-// /* this is a test    alert("window is resized");*/
-
-// $("#box-container div").each(function(){
-// var randomWidth = Math.random() * 300;
-// var randomHeight =Math.random() * 300;
-// var randomColorBoxes = colorPalette[Math.floor(Math.random() * colorPalette.length)];
-// $(this).css("width",randomWidth).css("height", randomHeight);
-
-// 	});
-// });
-
-/*this is for resizing as it expands*/
-
-//scroll to randomly change width and height of grid item 
-/*$(window).scroll(function()  {
-$("#box-container div").each(function(){
-	var randomColorBoxes = colorPalette[Math.floor(Math.random() * colorPalette.length)];
-	$(this).css("background-color", randomColorBoxes);
-	});
-}); */
-
-
-//KEYBOARD EVENTS
-var increaseWidth =10;
-var increaseHeight= 10;
 
 
 
-$(document).keydown(function(event){
-	var code= event.which; //keycode
-
-	if (code==65) { //the keycode for 'a' is 65
-	increaseWidth+=10
-	//increaseWidth=increaseWidth +1;
-	//simplified : increaseWidth ++
-		console.log("the 'a' key is down");
-
-		$("#box-container div").css("width",increaseWidth);
-
-	}else {
-		console.log("typing other letters");
-		increaseHeight+=10;
-		$("#box-container div").css("height",increaseHeight);
-
- //good for testing
-		
-	};
