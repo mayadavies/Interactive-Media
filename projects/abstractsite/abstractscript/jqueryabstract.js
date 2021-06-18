d4dce766d6343367dda6468b0be7888e2171ea73
maya davies <mayadavies@mayas-MacBook-Pro-3.local>
2021-06-18 11:30:13 -0400

    update

--- projects/abstractsite/abstractscript/jqueryabstract.js
+++ projects/abstractsite/abstractscript/jqueryabstract.js
@@ -1,25 +1,15 @@
 
 
+//declaring color palette background color
 
- var black="black";
-
-
-
-//declaring color palette in an array
- var colorPalette = [black];
-
-/*
-console.log(Math.random()); //Math.random creates a value between 0-1 (always a decimal)
-console.log(Math.floor(Math.random())); //math.floor rounds down to a nearest whole number
-console.log(Math.floor(Math.random() * 6)); */
-
-
+var black="black";
+var colorPalette = [black];
 var randomColor = Math.floor(Math.random() * colorPalette.length);$("body").css("background-color",colorPalette[randomColor]);
-//command '/'' makea a whole line a comment//
 
+//custom cursor settings
 let mouseCursor = document.querySelector(".cursor")
-let lis = document.querySelectorAll(".li")
 
+let lis = document.querySelectorAll(".li")
 window.addEventListener("mousemove", cursor)
 
 function cursor(e) {
@@ -31,25 +21,4 @@ function cursor(e) {
 
 
 
-$(document).hover(function(event){
-	var code= event.which; //keycode
-
-	if (code==65) { //the keycode for 'a' is 65
-	increaseWidth+=10
-	//increaseWidth=increaseWidth +1;
-	//simplified : increaseWidth ++
-		console.log("the 'a' key is down");
-
-		$("#box-container div").css("width",increaseWidth);
-
-	}else {
-		console.log("typing other letters");
-		increaseHeight+=10;
-		$("#box-container div").css("height",increaseHeight);
-
- //good for testing
-		
-	};
-
-});
 
