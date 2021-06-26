1de6303f27e11ef09fe1203d6923725a5b579041
mayadavies <mayahd.davies@gmail.com>
2021-06-26 18:33:01 -0400

    help

--- index/jquery.js
+++ index/jquery.js
@@ -5,27 +5,26 @@ Abstract website tutorial
 
 */
 //declaring colours
- var green="#CBFF00";
- var pink="#FF13BA";
- var purple="#7D00FF";
- var lightpink="#FF96D6";
- var orange="#FF7600";
- var white="#white";
- var black ="#black"
+ var red="#FF0000";
+ var yellow="#FFFF00";
+ var green="#81FF2B";
+ var cyan="#00FFFF";
+ var blue="#0000FF";
+ var magenta="#FF00FF";
+ var purple ="#662D91"
 
 //declaring color palette in an array
- var colorPalette = [green,pink,purple,lightpink,white,orange,black];
+ var colorPalette = [red,yellow,green,cyan,blue,magenta,purple];
+
+/*
+console.log(Math.random()); //Math.random creates a value between 0-1 (always a decimal)
+console.log(Math.floor(Math.random())); //math.floor rounds down to a nearest whole number
+console.log(Math.floor(Math.random() * 6)); */
 
 
-// console.log(Math.random()); //Math.random creates a value between 0-1 (always a decimal)
-// console.log(Math.floor(Math.random())); //math.floor rounds down to a nearest whole number
-console.log(Math.floor(Math.random() * 7)); 
+var randomColor = Math.floor(Math.random() * colorPalette.length);
 
-
-var randomColor = Math.floor(Math.random() * colorPalette = [green, pink]);
-
-console.log(colorPalette.length);
-
+console.log ("testing testing");
 //this is a test to see if my file is linked tomy html//
 
 
@@ -57,21 +56,28 @@ $("#box-container div").each(function(){
 
 });
 
+//randomly change width and height of grid item when clicked
+// $("#box-container div").click(function()  {
+// var randomWidth = Math.random() * 300;
+// var randomHeight =Math.random() * 300;
+// $(this).css("width",randomWidth).css("height", randomHeight);
 
+
+// });
 
 /*this is for resizing as it expands*/
 
-$( window ).resize(function() {
+// $( window ).resize(function() {
 // /* this is a test    alert("window is resized");*/
 
- $("#box-container div").each(function(){
- var randomWidth = Math.random() * 300;
- var randomHeight =Math.random() * 300;
- var randomColorBoxes = colorPalette[Math.floor(Math.random() * colorPalette.length)];
- $(this).css("width",randomWidth).css("height", randomHeight);
+// $("#box-container div").each(function(){
+// var randomWidth = Math.random() * 300;
+// var randomHeight =Math.random() * 300;
+// var randomColorBoxes = colorPalette[Math.floor(Math.random() * colorPalette.length)];
+// $(this).css("width",randomWidth).css("height", randomHeight);
 
-	});
-});
+// 	});
+// });
 
 /*this is for resizing as it expands*/
 
@@ -93,7 +99,7 @@ var increaseHeight= 10;
 $(document).keydown(function(event){
 	var code= event.which; //keycode
 
-	if (code==65) { //the keycode for 'a' is 65
+	if clicked { //the keycode for 'a' is 65
 	increaseWidth+=10
 	//increaseWidth=increaseWidth +1;
 	//simplified : increaseWidth ++
