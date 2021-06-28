acf8db26658a0c351241e7bc28767e9643d9c0b9
mayadavies <mayahd.davies@gmail.com>
2021-06-28 15:17:20 -0400

    update

--- index/jquery.js
+++ index/jquery.js
@@ -1,9 +1,4 @@
-/*
-Abstract website tutorial
 
-
-
-*/
 //declaring colours
  var green="#CBFF00";
  var pink="#FF13BA";
@@ -16,33 +11,20 @@ Abstract website tutorial
 //declaring color palette in an array
  var colorPalette = [green,purple,lightpink,orange,,black];
 
-/*
-console.log(Math.random()); //Math.random creates a value between 0-1 (always a decimal)
-console.log(Math.floor(Math.random())); //math.floor rounds down to a nearest whole number
-console.log(Math.floor(Math.random() * 6)); */
-
+//random background colour//
 
 var randomColor = Math.floor(Math.random() * colorPalette.length);
 
-console.log ("hi");
-//this is a test to see if my file is linked tomy html//
-
-
-//change bg color to green/
-
- //$("body").css("background-color",colorPalette[2]);
-
 $("body").css("background-color",colorPalette[randomColor]);
-//command '/'' makea a whole line a comment//
-//KEYBOARD EVENTS
 
 
+
+
+//button events
 
 function myFunction() {
   document.getElementById("demo").innerHTML = "I learned alot of beautiful ways to express myself throughout this class, thank you!";
 }
-
-
 
 
 function myFunction2() {
