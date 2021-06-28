17cfad6c1271fcebebf4295eba5f0e3e88a6c443
mayadavies <mayahd.davies@gmail.com>
2021-06-28 12:16:35 -0400

    changed

--- index/jquery.js
+++ index/jquery.js
@@ -34,91 +34,20 @@ console.log ("hi");
 
 $("body").css("background-color",colorPalette[randomColor]);
 //command '/'' makea a whole line a comment//
-
-
-$("button").click(function() {
-	console.log("Add Box");
-	$("#box-container").append("<div></div>");
-
-});
-
-
-// // For loop to create many boxes
-
-// //	$("#box-container").append("<div></div>");
-
-// };
-// //loop through all grid idems and add a random color from colorPallette
-// $("#box-container div").each(function(){
-// 	var randomColorBoxes = colorPalette[Math.floor(Math.random() * colorPalette.length)];
-// 	$(this).css("background-color", randomColorBoxes);
-
-// });
-
-//randomly change width and height of grid item when clicked
-// $("#box-container div").click(function()  {
-// var randomWidth = Math.random() * 300;
-// var randomHeight =Math.random() * 300;
-// $(this).css("width",randomWidth).css("height", randomHeight);
-
-
-// });
-
-/*this is for resizing as it expands*/
-
-// $( window ).resize(function() {
-// /* this is a test    alert("window is resized");*/
-
-// $("#box-container div").each(function(){
-// var randomWidth = Math.random() * 300;
-// var randomHeight =Math.random() * 300;
-// var randomColorBoxes = colorPalette[Math.floor(Math.random() * colorPalette.length)];
-// $(this).css("width",randomWidth).css("height", randomHeight);
-
-// 	});
-// });
-
-/*this is for resizing as it expands*/
-
-//scroll to randomly change width and height of grid item 
-/*$(window).scroll(function()  {
-$("#box-container div").each(function(){
-	var randomColorBoxes = colorPalette[Math.floor(Math.random() * colorPalette.length)];
-	$(this).css("background-color", randomColorBoxes);
-	});
-}); */
-
-
 //KEYBOARD EVENTS
-// var increaseWidth =10;
-// var increaseHeight= 10;
 
 
 
-// $(document).keydown(function(event){
-// 	var code= event.which; //keycode
+function myFunction() {
+  document.getElementById("demo").innerHTML = "I learned alot of beautiful ways to express myself throughout this class, thank you!";
+}
 
-// 	if clicked { //the keycode for 'a' is 65
-// 	increaseWidth+=10
-// 	//increaseWidth=increaseWidth +1;
-// 	//simplified : increaseWidth ++
-// 		console.log("the 'a' key is down");
 
-// 		$("#box-container div").css("width",increaseWidth);
 
-// 	}else {
-// 		console.log("typing other letters");
-// 		increaseHeight+=10;
-// 		$("#box-container div").css("height",increaseHeight);
 
-//  //good for testing
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "God, I really hope I pass this class.";
+}
+
 		
-// 	};
-
-// });
-
-
-
-
-
 
